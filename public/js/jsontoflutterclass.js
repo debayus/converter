b3aa7a6f5cd2b48e75909d8e7dab39e367ffdda4
Gede Bayu <bayusth@gmail.com>
2022-07-14 14:41:24 +0800

    init

---
+++ public/js/jsontoflutterclass.js
@@ -0,0 +1,148 @@
+const input_json = ace.edit('input_json');
+input_json.setTheme('ace/theme/monokai');
+input_json.getSession().setMode('ace/mode/json');
+input_json.setShowPrintMargin(false);
+
+const result_code = ace.edit('result_code');
+result_code.setShowPrintMargin(false);
+
+function isJsonString(str) {
+    try {
+        return({
+            success : true,
+            json : JSON.parse(str)
+        });
+    } catch (e) {
+        return {
+            success : false,
+        };
+    }
+}
+
+const formOnSubmit = e => {
+    e.preventDefault();
+
+    // class name
+    const className = e.target['className'].value;
+
+    // convert string to json
+    const isJson = isJsonString(input_json.getValue());
+    if (!isJson.success){
+        alert('Wrong Format');
+    }
+
+    // get params
+    const models = getModels(className, isJson.json);
+    const listClass = models.reverse().map(x => getClass(x)).join('\r\r');
+
+    let r = `import 'dart:convert';
+import 'package:flutter/material.dart';
+
+${listClass}
+`;
+    result_code.setValue(r);
+};
+
+const getModels = (className, json) => {
+    const models = [];
+    const _getModel = (_className, _json) => {
+        const keys = Object.keys(_json);
+        const props = [];
+        const dynamics = [];
+        for(let i = 0; i < keys.length; i++){
+            const key = keys[i];
+            const val = _json[key];
+            const propName = getPropName(key);
+            props.push(`\t${getVariable(_className, key, val)} ${propName};`);
+            if (Array.isArray(val) && val?.length > 0){
+                const classNamex = `${className}${getPropName(key, true)}`;
+                models.push(_getModel(classNamex, val[0]));
+                let rx = '';
+                rx += `\t\tif (dynamicData['${key}'] != null) {\r`;
+                rx += `\t\t\tfinal detailT = dynamicData['${key}'] as List;\r`;
+                rx += `\t\t\tmodel.${propName} = [];\r`;
+                rx += `\t\t\tfor (var i = 0; i < detailT.length; i++) {\r`;
+                rx += `\t\t\tmodel.${propName}!.add(${classNamex}.fromDynamic(detailT[i]));\r`;
+                rx += `\t\t\t}\r`;
+                rx += `\t\t}`;
+                dynamics.push(rx);
+            } else{
+                dynamics.push(`\t\tmodel.${propName} = ${getDynamicData(_className, key, val)};`);
+            }
+        }
+        return {
+            className : _className,
+            props : props,
+            dynamics : dynamics
+        };
+    };
+    models.push(_getModel(className, json));
+    return models;
+};
+
+const getPropName = (v, sentenceCase = false) => {
+    if (!v) return v;
+    v = v.split('_').map(x => toSentenceCase(x)).join('');
+    if (!sentenceCase){
+        v = v[0].toLowerCase() + v.substr(1);
+    }
+    return v;
+};
+
+const getDynamicData = (className, key, val) => {
+    const variable = getVariable(className, key, val);
+    const dynamic = `dynamicData['${key}']`;
+    if (variable === 'DateTime?'){
+        return `Helper.stringToDate(${dynamic})`;
+    }else{
+        return dynamic;
+    }
+};
+
+const getVariable = (className, key, v) => {
+    if (v === 0){
+        return 'int?';
+    } else if (v === true){
+        return 'bool?';
+    } else if (v === 'string'){
+        return 'String?';
+    } else if (!isNaN(new Date(v))){
+        return 'DateTime?'
+    } else if (v?.ticks === 0) {
+        return 'TimeOfDay?'
+    } else if (Array.isArray(v)) {
+        return `List<${className}${getPropName(key, true)}Model>?`;
+    } else {
+        return 'String?';
+    }
+};
+
+const getClass = m => {
+    const r = `class ${m.className}Model {
+
+${m.props.join('\r')}
+
+    static fromJson(String jsonString) {
+        final data = json.decode(jsonString);
+        return fromDynamic(data);
+    }
+
+    static fromDynamic(dynamic dynamicData) {
+        final model = ${m.className}();
+
+${m.dynamics.join('\r')}
+
+        return model;
+    }
+}`;
+    return r;
+};
+
+function toSentenceCase(str) {
+    return str.replace(
+      /\w\S*/g,
+      function(txt) {
+        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+      }
+    );
+  }
---
+++ resources/views/jsonToFlutterClass/index.blade.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+  <title>Json To Flutter Class - Mahas</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link href="/css/jsontoflutterclass.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+    <nav class="navbar navbar-dark bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Json To Flutter Class</a>
+        </div>
+    </nav>
+    <div class="main">
+        <div class="menu">
+            <form onsubmit="formOnSubmit(event)">
+                <input type="text" class="form-control rounded-0" required name="className" placeholder="Class Name" value="Sample" />
+                <pre id="input_json" data-fouc>{
+    "number": 0,
+    "text": "string",
+    "boolean": true,
+    "datetime": "2022-07-14T05:24:03.327Z",
+    "time": {
+        "ticks": 0,
+        "days": 0,
+        "hours": 0,
+        "milliseconds": 0,
+        "minutes": 0,
+        "seconds": 0,
+        "totalDays": 0,
+        "totalHours": 0,
+        "totalMilliseconds": 0,
+        "totalMinutes": 0,
+        "totalSeconds": 0
+    },
+    "list": [
+        {
+            "number": 0,
+            "text": "string",
+            "sub_list": [
+                {
+                    "number": 0,
+                    "text": "string"
+                }
+            ]
+        }
+    ]
+}</pre>
+                <button class="btn btn-block btn-primary rounded-0" type="submit">PROCESS</button>
+            </form>
+        </div>
+        <div class="result">
+            <button class="btn btn-block btn-primary rounded-0" type="button">COPY</button>
+            <pre id="result_code" data-fouc>import 'dart:convert';
+
+class ProfileRekeningModel {
+    int? id;
+    int? idPegawai;
+    String? nomor;
+    String? bank;
+    String? atasNama;
+    bool? aktif;
+    bool? prioritas;
+
+    static fromJson(String jsonString) {
+        final data = json.decode(jsonString);
+        return fromDynamic(data);
+    }
+
+    static fromDynamic(dynamic dynamicData) {
+        final model = ProfileRekeningModel();
+        model.id = dynamicData['id'];
+        model.idPegawai = dynamicData['id_Pegawai'];
+        model.nomor = dynamicData['nomor'];
+        model.bank = dynamicData['bank'];
+        model.atasNama = dynamicData['atasNama'];
+        model.aktif = dynamicData['aktif'];
+        model.prioritas = dynamicData['prioritas'];
+        return model;
+    }
+}</pre>
+        </div>
+    <div>
+
+    <script src="/js/ace/ace.js" type="text/javascript"></script>
+    <script src="/js/jsontoflutterclass.js" type="text/javascript"></script>
+</body>
+</html>
